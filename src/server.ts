b0d7ac5437cd0ecc99656b76d6ddd4abeb4b3524
rahul_thakur <rahulthakur.developer@gmail.com>
2024-10-31 08:08:34 +0530

    test lint-staged

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,10 @@
 function welcome(name: string) {
-    return `Welcome ${name}`
+    const person1 = {
+        name: 'Rahul',
+        age: 30,
+    }
+    const personName = person1.name
+    return `Welcome ${name}` + personName
 }
 
 welcome('John Doe')
