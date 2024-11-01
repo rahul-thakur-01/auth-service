fe476c301566e9365d19100dbbad132b53cd99d2
rahul_thakur <rahulthakur.developer@gmail.com>
2024-11-01 18:13:23 +0530

    update user register-validator

--- src/validators/register-validator.ts
+++ src/validators/register-validator.ts
@@ -2,26 +2,41 @@ import { checkSchema } from 'express-validator'
 
 export default checkSchema({
     email: {
-        errorMessage: 'Email is required !',
+        trim: true,
+        errorMessage: 'Email is required!',
+        notEmpty: true,
+        isEmail: {
+            errorMessage: 'Email should be a valid email',
+        },
+    },
+    firstName: {
+        errorMessage: 'First name is required!',
+        notEmpty: true,
+        trim: true,
+    },
+    lastName: {
+        errorMessage: 'Last name is required!',
         notEmpty: true,
         trim: true,
     },
     password: {
+        trim: true,
+        errorMessage: 'Last name is required!',
+        notEmpty: true,
         isLength: {
-            options: { min: 8 },
-            errorMessage: 'Password should be atleast 8 characters long !',
+            options: {
+                min: 8,
+            },
+            errorMessage: 'Password length should be at least 8 chars!',
         },
     },
 })
 
 // import { body } from "express-validator";
 // export default [
-
-//     body("email")
-//     .notEmpty().withMessage("Email is required !"),
+//     body("email").notEmpty().withMessage("Email is required !"),
 
 //     body("firstName")
 //     .notEmpty().withMessage("First name is required !")
 //     .isAlpha().withMessage("First name should contain only alphabets !"),
-
 // ];
