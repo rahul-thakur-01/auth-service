f6764bc1735a535a0cf25e2d6c1e2289ce70d061
rahul_thakur <rahulthakur.developer@gmail.com>
2024-11-02 10:30:33 +0530

    setup static file to host public key

--- src/app.ts
+++ src/app.ts
@@ -8,6 +8,7 @@ const app = express()
 
 app.use(express.json())
 app.use(cookieParser())
+app.use(express.static('public'))
 
 app.get('/', (req, res) => {
     res.status(200).json({ message: 'Hello World' })
